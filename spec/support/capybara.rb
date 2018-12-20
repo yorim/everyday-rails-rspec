fc083c380cf5cb0fdfb92b0823d2bd2b4e0151ce
yorim <sasuraino12@gmail.com>
2018-12-21 00:51:34 +0900

    modify config.yml 7

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,7 +3,7 @@ require 'capybara/rspec'
 require 'selenium-webdriver'
 
 client = Selenium::WebDriver::Remote::Http::Default.new
-client.read_timeout = 120 #instead of the default 60
+client.read_timeout = 180 #instead of the default 60
 
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(
