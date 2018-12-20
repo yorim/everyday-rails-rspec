2de46065bdc483f5e213719b8a9d61aab61410d0
yorim <sasuraino12@gmail.com>
2018-12-20 23:30:00 +0900

    modify config.yml 6

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,1 +1,22 @@
-Capybara.javascript_driver = :selenium_chrome_headless
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'selenium-webdriver'
+
+client = Selenium::WebDriver::Remote::Http::Default.new
+client.read_timeout = 120 #instead of the default 60
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(
+                                app,
+                                browser: :chrome,
+                                desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+                                                                                                   chrome_options: {
+                                                                                                       args: ['headless',
+                                                                                                       'window-size=1280,768'],
+                                                                                                   },
+                                ),
+                                http_client: client,
+  )
+end
+
+Capybara.javascript_driver = :selenium
