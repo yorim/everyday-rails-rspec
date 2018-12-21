01b6c9573320ad025e49cc68f734cd1006632511
yorim <sasuraino12@gmail.com>
2018-12-21 19:13:40 +0900

    add port on capybara.rb

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -12,7 +12,7 @@ Capybara.register_driver :selenium do |app|
                                 desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
                                                                                                    chrome_options: {
                                                                                                        args: ['headless',
-                                                                                                       'window-size=1280,768'],
+                                                                                                       'window-size=1280,768' remote-debugging-port=4444],
                                                                                                    },
                                 ),
                                 http_client: client,
