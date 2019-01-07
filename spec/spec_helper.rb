3abdbdf51b5f174ec2b3fdff34770350b2e211cb
yorim <sasuraino12@gmail.com>
2019-01-07 20:39:51 +0900

    remove retry_method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,9 +50,9 @@ RSpec.configure do |config|
   config.display_try_failure_messages = true
 
   #run retry only on features
-  config.around :each, :js do |ex|
-    ex.run_with_retry retry:2
-  end
+  # config.around :each, :js do |ex|
+  #   ex.run_with_retry retry:2
+  # end
 
   # callback to be run between retries
   config.retry_callback = proc do |ex|
